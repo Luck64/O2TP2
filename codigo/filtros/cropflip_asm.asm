ed8ddeb4ac4498c630317908a1474073fcef71b5
Emiliano <noggaxiii@gmail.com>
2016-04-29 13:51:18 -0300

    Ahora si, el cropflip esta 10/10. Eso si, tiene mas comentarios que la mierda y es dificil leerlo. je

--- codigo/filtros/cropflip_asm.asm
+++ codigo/filtros/cropflip_asm.asm
@@ -46,27 +46,28 @@ cropflip_asm:
 	xor r13, r13
 	xor r14, r14
 	xor r15, r15	
-
+					;RDX = columnas
 	mov ebx, [rbp+16] ;ebx = tamx
 	mov r12d, [rbp+24] ; r12d = tamy
 	mov r13d, [rbp+32] ; r13d = offsetx
 	mov r14d, [rbp+40] ; r14d = offsety
 
 	;primero tengo que posicionar el puntero a (offsetx,offsety), luego ponerlo en la primera celda superior izquierda.
-	lea r15, [r12+r14]
-	dec r15
-	lea rdx, [rdx*4]
-	lea r13, [r13*4]
-	lea rbx, [rbx*4]
-	.primeraCeldaSupIzq:
-	cmp r8, r15
-	je .setOffsetX
-	add rdi, rdx
-	inc r8
-	jmp .primeraCeldaSupIzq
-
-	.setOffsetX:
-	add rdi, r13
+	lea r15, [r12+r14]	;tamy+offsety
+	dec r15				;tamy+offsety-1
+	lea rdx, [rdx*4]	;COLS,
+	mov rcx, rdx 		;me guardo COLS*4 en rcx, ya que lo va a pisar mult
+	mov eax, r15d
+	mul ecx				;me deja el resultado de [r15*rdx], o sea[(offsety+tamy-1)*(COLS*4)], en edx y eax
+	add eax, edx
+	mov r15, rax 
+	
+	lea r13, [r13*4]	;OFFSETX, lo veo de a 4
+	;lea rbx, [rbx*4]	;TAMX, lo veo de a 4
+	add r15, r13
+	add r15, rdi
+	mov rdi, r15      	;rdi esta apuntando a la primera celda de la fila mas alta 
+	;add rdi, r13 		;desde (0,0) salto a (offsetx, 0)
 	jmp .cicloX
     
 
@@ -75,15 +76,16 @@ cropflip_asm:
 	inc r10
 	cmp r10, r12	;comparo si el contador llego a tamy
 	je .fin
-	sub rdi, rdx ;Llegue al final de la fila. Ahora resto una tira de columnas entera (o sea, bajo de fila) y resto el tamx. entonces ahora estoy al principio de la fila de abajo, respetando el offset
-	sub rdi, r13
+	sub r15, rcx
+
+	mov rdi, r15
 
     .cicloX:
     movdqu xmm0, [rdi] 		 ; XMM0 = [ B1 | G1 | R1 | A1 | B2 | G2 | R2 | A2 | B3 | G3 | R3 | A3 | B4 | G4 | R4 | A4 ]
 	movdqu [rsi], xmm0		 ; XMM0 = [ B1 | G1 | R1 | A1 | B2 | G2 | R2 | A2 | B3 | G3 | R3 | A3 | B4 | G4 | R4 | A4 ]
 	lea rsi, [rsi + 16]
 	lea rdi, [rdi + 16]
-	add r9, 16
+	add r9, 4
 	;inc r9
 	cmp r9, rbx ; comparo para ver si ya termine esta fila (con tamx)
 	jge .cicloY
@@ -99,3 +101,37 @@ cropflip_asm:
 	pop rbx
 	pop rbp
     ret
+
+
+
+; .primeraCeldaSupIzq:
+; 	cmp r8, r15
+; 	je .setOffsetX
+; 	add rdi, rdx
+; 	inc r8
+; 	jmp .primeraCeldaSupIzq
+
+; 	.setOffsetX:
+; 	add rdi, r13
+; 	jmp .cicloX
+    
+
+; 	.cicloY:
+; 	xor r9, r9 ; voy a usarlo como contador de columnas
+; 	inc r10
+; 	cmp r10, r12	;comparo si el contador llego a tamy
+; 	je .fin
+; 	sub rdi, rdx ;Llegue al final de la fila. Ahora resto una tira de columnas entera (o sea, bajo de fila) 
+; 	sub rdi, r13 ;y resto el tamx. entonces ahora estoy al principio de la fila de abajo, respetando el offset
+
+;     .cicloX:
+;     movdqu xmm0, [rdi] 		 ; XMM0 = [ B1 | G1 | R1 | A1 | B2 | G2 | R2 | A2 | B3 | G3 | R3 | A3 | B4 | G4 | R4 | A4 ]
+; 	movdqu [rsi], xmm0		 ; XMM0 = [ B1 | G1 | R1 | A1 | B2 | G2 | R2 | A2 | B3 | G3 | R3 | A3 | B4 | G4 | R4 | A4 ]
+; 	lea rsi, [rsi + 16]
+; 	lea rdi, [rdi + 16]
+; 	add r9, 4
+; 	;inc r9
+; 	cmp r9, rbx ; comparo para ver si ya termine esta fila (con tamx)
+; 	jge .cicloY
+; 	;je .cicloY
+; 	jmp .cicloX	
