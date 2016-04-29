d7bb445ef0525a53c6cd438b6eceb3b2853381ca
Maria Florencia Lagos amartino <flagos@ws3.labo5>
2016-04-28 21:28:43 -0300

    ldr nuevo

--- codigo/filtros/ldr_asm.asm
+++ codigo/filtros/ldr_asm.asm
@@ -42,14 +42,28 @@ ldr_asm:
 	push R14				;Desalineada
 	push RBX				;Alineada
 
-	xor r12, r12								; R12 = iterador de vecinos
 	xor r14, r14								; R14 = iterador de pixeles
-	;add r14, cols
-	;add r14, cols
+	add r14, rcx
+	add r14, rcx
 	add r14, 2									; R14 = 2*cols + 2 (inicio)
 	xor r13, r13
-	mov r13B, [RBP + 16]				;R13 = alpha
+	mov r13B, [RBP + 16]				; R13 = alpha
 
+.vecinos:
+	xor r10, r10								; R10 iteracion de vecinos 
+	xor r12, r12								; R12 = iterador de vecinos
+;CORREGIR	lea r12, [r14-rcx-rcx-2]		; posicion inicial de it e vecinos
+	JMP .vecinosUp
+
+.incItPixeli:
+;Corregir	CMP r14, 0												; ¿Llegue al margen derecho?
+	add r14, 4
+	JMP .vecinos 
+
+.vecinosUp:
+
+	CMP r10, 5									; 5 es la cant de filas vecinas a tomar
+;CRREGIR	JE .incItPixel
 ;vecinos:           pixeles:
 ;|U|V|W|X|Y|				| | | | | | | | |
 ;|P|Q|R|S|T|				| | | | | | | | |
@@ -67,26 +81,21 @@ ldr_asm:
 ;XMM1 = [    B1     |    B2     |    B3     |     B4    ]
 ;XMM1 = [B2|G2|R2|a2|B3|G3|R3|a3|B4|G4|R4|a4|B5|G5|R5|a5]
 	movdqu xmm1, [rdi + PIXEL_SIZE*r12]
-	movdqu xmm11, xmm1
 	inc r12
 ;XMM2 = C
 ;XMM2 = [    C1     |    C2     |    C3     |     C4    ]
 ;XMM2 = [B3|G3|R3|a3|B4|G4|R4|a4|B5|G5|R5|a5|B6|G6|R6|a6]
 	movdqu xmm2, [rdi + PIXEL_SIZE*r12]
-	movdqu xmm12, xmm2
 	inc r12
 ;XMM3 = D
 ;XMM3 = [    D1     |    D2     |    D3     |     D4    ]
 ;XMM3 = [B4|G4|R4|a4|B5|G5|R5|a5|B6|G6|R6|a6|B7|G7|R7|a7]
 	movdqu xmm3, [rdi + PIXEL_SIZE*r12]
-	movdqu xmm13, xmm3
 	inc r12
 ;XMM4 = E
 ;XMM4 = [    E1     |    E2     |    E3     |     E4    ]
 ;XMM4 = [B5|G5|R5|a5|B6|G6|R6|a6|B7|G7|R7|a7|B8|G8|R8|a8]
 	movdqu xmm4, [rdi + PIXEL_SIZE*r12]
-	movdqu xmm14, xmm4
-
 .suma:
 	movdqu xmm15, [mascSuma1]
 	
@@ -138,9 +147,9 @@ ldr_asm:
 	
 ; XMM0 = [Sumargb1|Sumargb2]
 	movdqu xmm1, xmm0		; XMM0 = [Sb1|Sg1|Sr1| 0 |Sb2|Sg2|Sr2| 0 ]
-	psrldq xmm1, 2			; XMM1 = [Sg1|Sr1| 0 |Sb2|Sg2|Sr2| 0 | 0 ]
+	pslldq xmm1, 2			; XMM1 = [Sg1|Sr1| 0 |Sb2|Sg2|Sr2| 0 | 0 ]
 	movdqu xmm2, xmm1
-	psrldq xmm2,2				; XMM2 = [Sr1| 0 |Sb2|Sg2|Sr2| 0 | 0 | 0 ]
+	pslldq xmm2,2				; XMM2 = [Sr1| 0 |Sb2|Sg2|Sr2| 0 | 0 | 0 ]
 	movdqu xmm15, [quitarBasura]
 	pand xmm0, xmm15		; XMM0 = [Sb1| 0 | 0 | 0 |Sb2| 0 | 0 | 0 ]
 	pand xmm1, xmm15		; XMM1 = [Sg1| 0 | 0 | 0 |Sg2| 0 | 0 | 0 ]
@@ -151,16 +160,26 @@ ldr_asm:
 
 ; XMM5 = [Sumargb3|Sumargb4]
 	movdqu xmm6, xmm5		; XMM5 = [Sb3|Sg3|Sr3| 0 |Sb4|Sg4|Sr4| 0 ]
-	psrldq xmm6, 2			; XMM6 = [Sg3|Sr3| 0 |Sb4|Sg4|Sr4| 0 | 0 ]
+	pslldq xmm6, 2			; XMM6 = [Sg3|Sr3| 0 |Sb4|Sg4|Sr4| 0 | 0 ]
 	movdqu xmm7, xmm6
-	psrldq xmm7,2				; XMM7 = [Sr3| 0 |Sb4|Sg4|Sr4| 0 | 0 | 0 ]
+	pslldq xmm7,2				; XMM7 = [Sr3| 0 |Sb4|Sg4|Sr4| 0 | 0 | 0 ]
 	paddd xmm5, xmm15		; XMM5 = [Sb3| 0 | 0 | 0 |Sb4| 0 | 0 | 0 ]
 	paddd xmm6, xmm15		; XMM6 = [Sg3| 0 | 0 | 0 |Sg4| 0 | 0 | 0 ]
 	paddd xmm7, xmm15		; XMM7 = [Sr3| 0 | 0 | 0 |Sr4| 0 | 0 | 0 ]
 	; suma:
 	paddd xmm5, xmm6		; XMM5 = [Sumargb3|0|Sumargb4|0]
 	paddd xmm5, xmm7
-
+	
+; XMM5 = [Sumargb1|Sumargb3|Sumargb2|Sumargb4]
+	psrldq xmm5,4
+	pand xmm5, xmm0
+	
+;Sube una fila el indice
+	lea r12, [r12 + rcx - 4]
+	inc r10 
+	JMP .vecinosUp
+;convertir: 
+;shit derecho y pand para pasar todo a un registro
 ; -- Convertir a float --
 	cvtps2dq xmm1, xmm0
 	cvtps2dq xmm2, xmm5
